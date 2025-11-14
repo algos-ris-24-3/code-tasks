d902b83523dd212a442de0c91dfcf6e95d8fc700
Dmitriy Nikitin <dn165941@gmail.com>
2025-11-14 18:30:22 +0500

    gcd_iterative_slow

--- main.py
+++ main.py
@@ -26,7 +26,18 @@ def gcd_iterative_slow(a: int, b: int) -> int:
     :param b: целое число b
     :return: значение наибольшего общего делителя
     """
-    pass
+    a = abs(a)
+    b = abs(b)
+    if a == 0:
+        return b
+    if b == 0:
+        return a
+    while a != b:
+        if a > b:
+            a -= b
+        else:
+            b -= a
+    return a
 
 
 def gcd_iterative_fast(a: int, b: int) -> int:
