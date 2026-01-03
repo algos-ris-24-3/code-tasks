591b5d230a89618723ef3b6d2a5da749d7ec7d06
Angelina Gradoboeva <aagradoboeva@edu.hse.ru>
2026-01-04 00:30:24 +0500

    get_min_cost_path without consideration of None-cells

--- main.py
+++ main.py
@@ -21,8 +21,47 @@ def get_min_cost_path(
     cost - стоимость минимального пути,
     path - путь, список кортежей с индексами ячеек.
     """
-    pass
+    row_count = len(price_table) 
+    col_count = len(price_table[0])
 
+    path_cost = [[INF] * (col_count + 1) for _ in range(row_count + 1)]
+
+    path_cost[1][1] = price_table[0][0]
+
+    for j in range(2, col_count):
+        path_cost[1][j] = path_cost[1][j-1] + price_table[0][j-1]
+
+    for i in range(2, row_count):
+        path_cost[i][1] = path_cost[i-1][1] + price_table[i-1][0]
+
+    for row_idx in range(1, row_count + 1):
+        for col_idx in range (1, col_count + 1):
+            if row_idx == 1 and col_idx == 1:
+                continue
+            else:
+                price_table_value = price_table[row_idx - 1][col_idx - 1]
+                min_cost = min(path_cost[row_idx - 1][col_idx], path_cost[row_idx ][col_idx - 1])
+                path_cost[row_idx][col_idx] = min_cost + price_table_value
+                
+    path = []
+    i, j = row_count, col_count
+    while i > 1 or j > 1:
+        path.append((i-1, j-1))
+        if i > 1 or j > 1:
+            if path_cost [i - 1][j] <= path_cost [i][j - 1]:
+                i -= 1
+            else:
+                j -= 1
+        elif i > 1:
+            i -= 1
+        else:
+            j -= 1
+
+    path.append((0, 0))
+    path.reverse()
+    min_cost = path_cost[row_count][col_count]
+
+    return Result(cost=min_cost, path=path)
 
 def main():
     table = [[1, 2, 2], [3, 4, 2], [1, 1, 2]]
