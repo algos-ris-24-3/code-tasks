6f8249c7fac4b01aae055fa812f08a3a1f09d7a2
Лиля <lilia586isaeva@yandex.ru>
2026-01-05 16:56:55 +0500

    Валидация матрицы

--- main.py
+++ main.py
@@ -6,6 +6,32 @@ PARAM_ERR_MSG = "Таблица цен не является прямоугол�
 
 Result = namedtuple("Result", ["cost", "path"])
 
+def validate_matrix(price_table):
+    
+    if not isinstance(price_table, list):
+        raise ValueError(PARAM_ERR_MSG)
+
+    if not price_table:
+        raise ValueError(PARAM_ERR_MSG)
+    
+    for row in price_table:
+        if type(row) is not list:
+            raise ValueError(PARAM_ERR_MSG)
+
+    if not price_table[0]:
+        raise ValueError(PARAM_ERR_MSG)
+    
+    for row in price_table:
+        for cell in row:
+            if not isinstance(cell, (int, float)):
+                raise ValueError(PARAM_ERR_MSG)
+    
+    columns_count = len(price_table[0])
+    for row in price_table:
+        if not len(row) == columns_count:
+            raise ValueError(PARAM_ERR_MSG)
+    
+
 
 def get_min_cost_path(
     price_table: list[list[float | int]],
@@ -21,8 +47,16 @@ def get_min_cost_path(
     cost - стоимость минимального пути,
     path - путь, список кортежей с индексами ячеек.
     """
-    pass
+    
+    validate_matrix(price_table)
 
+    rows_count = len(price_table)
+    columns_count = len(price_table[0])
+    
+    if rows_count == 1 and columns_count == 1:
+        return Result(cost = float(price_table[0][0]), path = [(0, 0)])
+    
+    return Result(cost = 0.0, path = [])
 
 def main():
     table = [[1, 2, 2], [3, 4, 2], [1, 1, 2]]
