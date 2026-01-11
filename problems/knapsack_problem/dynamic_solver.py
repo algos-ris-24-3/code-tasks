05c2d88d759fc8f8e7a1921644f53159f1eb635a
Chelovek-MV <vvmaks06@mail.ru>
2026-01-11 18:31:47 +0500

    Co-authored-by: cu <yupyui@users.noreply.github.com>

--- problems/knapsack_problem/dynamic_solver.py
+++ problems/knapsack_problem/dynamic_solver.py
@@ -12,7 +12,35 @@ class DynamicSolver(KnapsackAbstractSolver):
         :return: максимально возможная общая стоимость и список индексов выбранных предметов
         :rtype: KnapsackSolution
         """
-        pass
+        n = self.item_cnt 
+        W = self.weight_limit
+        
+        dp = [[0] * (W + 1) for _ in range(n + 1)]
+        
+        for i in range(1, n + 1):
+            weight_i = self.weights[i - 1]
+            cost_i = self.costs[i - 1]
+            
+            for w in range(0, W + 1):
+                if weight_i <= w:
+                    dp[i][w] = max(dp[i - 1][w], cost_i + dp[i - 1][w - weight_i])
+                else:
+                    dp[i][w] = dp[i - 1][w]
+        
+        max_cost = dp[n][W]
+        
+        selected_items = []
+        w = W
+        
+        for i in range(n, 0, -1):
+
+            if dp[i][w] != dp[i - 1][w]:
+                selected_items.append(i - 1)
+                w -= self.weights[i - 1]
+        
+        selected_items.reverse()
+        
+        return KnapsackSolution(cost=max_cost, items=selected_items)
 
 
 if __name__ == "__main__":
