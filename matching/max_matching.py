64faaf204d1cbd5d79a801a565ecc0f147376b08
Egor Proskuriakov <proskurakov_egor@mail.ru>
2026-02-02 02:31:17 +0500

    task done

--- matching/max_matching.py
+++ matching/max_matching.py
@@ -1,7 +1,8 @@
+from collections import deque
+
 from matching.bipartite_graph import BipartiteGraph
 from matching.bipartite_graph_matching import BipartiteGraphMatching
 from matching.errors.error_message_enum import ErrorMessageEnum
-from collections import deque
 
 
 def get_max_matching(bipartite_graph: BipartiteGraph) -> BipartiteGraphMatching:
@@ -33,21 +34,54 @@ def get_max_matching(bipartite_graph: BipartiteGraph) -> BipartiteGraphMatching:
     return matching
 
 def alternating_chain_search(graph, match, size):
-    pass
+    left_side = [-1] * size
+
+    right_side = [-1] * size
+
+    vertices = deque()
+
+    for left in range(size):
+        if not match.is_left_covered(left):
+            left_side[left] = -2
+            vertices.append(left)
+
+    while len(vertices) > 0:
+        current_left = vertices.popleft()
+
+        for right in graph.right_neighbors(current_left):
+            if right_side[right] == -1:
+                right_side[right] = current_left
+
+                if not match.is_right_covered(right):
+                    return left_side, right_side, right
+                
+                left_vertex = match.get_left_match(right)
+
+                if left_side[left_vertex] == -1:
+                    left_side[left_vertex] = right
+                    vertices.append(left_vertex)
+
+    return None
 
 def increase_matching(match, alternating_chain):
-    for i in range(0, len(alternating_chain), 2):
-        left_vertex = alternating_chain[i][1]
-        right_vertex = alternating_chain[i+1][1]
+    left_side, right_side, last_right = alternating_chain
+
+    right_vertex = last_right
+
+    while True:
+        left_vertex = right_side[right_vertex]
+
         if match.is_left_covered(left_vertex):
             old_right = match.get_right_match(left_vertex)
+
             match.remove_edge(left_vertex, old_right)
 
-        if match.is_right_covered(right_vertex):
-            old_left = match.get_left_match(right_vertex)
-            match.remove_edge(old_left, right_vertex)
         match.add_edge(left_vertex, right_vertex)
-    
+
+        if left_side[left_vertex] == -2:
+            break
+
+        right_vertex = left_side[left_vertex]
 
 if __name__ == "__main__":
     print("Исходный двудольный граф")
